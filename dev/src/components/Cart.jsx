7c5e1ad63801b8b0794a5a03338f499c5468d5d3
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-11-21 17:05:47 +0000

    context apply across components

--- dev/src/components/Cart.jsx
+++ dev/src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import {useContext} from 'react';
 import {CartContext} from '../store/shopping-cart-context.jsx';
 
-export default function Cart({onUpdateItemQuantity}) {
-	const {items} = useContext(CartContext);
+export default function Cart() {
+	const {items, updateItemQuantity} = useContext(CartContext);
 	const totalPrice = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
 	const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
 
@@ -11,7 +11,7 @@ export default function Cart({onUpdateItemQuantity}) {
 			{items.length === 0 && <p>No items in cart!</p>}
 			{items.length > 0 && (
 				<ul id='cart-items'>
-					{context.items.map((item) => {
+					{items.map((item) => {
 						const formattedPrice = `$${item.price.toFixed(2)}`;
 
 						return (
@@ -21,11 +21,11 @@ export default function Cart({onUpdateItemQuantity}) {
 									<span> ({formattedPrice})</span>
 								</div>
 								<div className='cart-item-actions'>
-									<button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+									<button onClick={() => updateItemQuantity(item.id, -1)}>
 										-
 									</button>
 									<span>{item.quantity}</span>
-									<button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+									<button onClick={() => updateItemQuantity(item.id, 1)}>
 										+
 									</button>
 								</div>
--- dev/src/store/shopping-cart-context.jsx
+++ dev/src/store/shopping-cart-context.jsx
@@ -3,4 +3,7 @@ import {createContext} from 'react';
 // may be any type od data passed into
 export const CartContext = createContext({
 	items: [],
+	// for help in autocompletion
+	addItemToCart: () => {},
+	updateItemQuantity: () => {},
 });
